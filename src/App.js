074f0efe05f51da3d47706d599a9f8b8c77cd247
Saba Komladze <sabakomladze2@gmail.com>
2023-05-18 22:52:19 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { SinglePost } from "./SinglePost";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="header">
+        <div className="leftside">
+          <h1>Notifications</h1>
+        </div>
+        <p>Mark all as read</p>
+      </section>
+      {SinglePost.map((object) => console.log(object.author.name))}
     </div>
   );
 }
---
+++ src/SinglePost.js
@@ -0,0 +1,91 @@
+export const SinglePost = [
+  {
+    author: {
+      name: "Mark Webber",
+      img: "./assets/avatar-mark-webber.webp",
+    },
+
+    text: "reacted to your recent post",
+    msg: "My first tournament today!",
+    time: "1m ago",
+    read: false,
+  },
+  {
+    author: {
+      name: "Angela Gray",
+      img: "./assets/avatar-angela-gray.webp",
+    },
+
+    text: "avatar-angela-gray.webp",
+    time: "5m ago",
+    read: false,
+  },
+  {
+    author: {
+      name: "Jacob Thompson",
+      img: "./assets/avatar-jacob-thompson.webp",
+    },
+
+    text: "has joined your group",
+    msg: "Chess Club",
+    time: "1 day ago",
+    read: false,
+  },
+
+  {
+    author: {
+      name: "Mark Webber",
+      img: "./assets/avatar-mark-webber.webp",
+    },
+
+    text: "reacted to your recent post",
+    msg: "My first tournament today!",
+    time: "1m ago",
+    read: false,
+  },
+
+  {
+    author: {
+      name: "Rizky Hasanuddin",
+      img: "./assets/avatar-rizky-hasanuddin.webp",
+    },
+
+    text: "sent you a private message",
+    time: "1 day ago",
+    msg: "My first tournament today!",
+    read: true,
+  },
+  {
+    author: {
+      name: "Kimberly Smith",
+      img: "./assets/avatar-kimberly-smith.webp",
+    },
+
+    text: "commented on your picture",
+    time: "1 week ago",
+    read: true,
+  },
+  {
+    author: {
+      name: "Nathan Peterson",
+      img: "./assets/avatar-nathan-peterson.webp",
+    },
+
+    text: "reacted to your recent post",
+    msg: "5 end-game strategies to increase your win rate",
+    time: "2 weeks ago",
+    read: true,
+  },
+
+  {
+    author: {
+      name: "Anna Kim",
+      img: "./assets/avatar-anna-kim.webp",
+    },
+
+    text: "left the group",
+    msg: "Chess Club",
+    time: "2 weeks ago",
+    read: true,
+  },
+];
