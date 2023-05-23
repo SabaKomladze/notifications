89f46e7b490ef683e56a52384dbf7633903e4dc8
Saba Komladze <sabakomladze2@gmail.com>
2023-05-23 19:11:08 +0400

    last

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
 import "./App.css";
 import { datalist } from "./SinglePost";
 import Posts from "./Posts";
-
+import { useState } from "react";
 function App() {
+  const [read, setRead] = useState(false);
+  const [number, setNumber] = useState(3);
   return (
     <div className="App">
       <div className="container">
         <header>
           <div className="leftside">
             <h1>Notifications</h1>
-            <div className="number">3</div>
+            <div className="number">{number}</div>
           </div>
-          <p className="mark">Mark all as read</p>
+          <p
+            className="mark"
+            onClick={() => {
+              for (let i = 0; i < datalist.length; i++) {
+                datalist[i].read = true;
+              }
+              if (number == 0) {
+                setNumber((prevNum) => prevNum - 0);
+              } else {
+                setNumber((prevNum) => prevNum - 3);
+              }
+            }}
+          >
+            Mark all as read
+          </p>
         </header>
         {datalist.map((data, index) => (
-          <Posts key={index} data={data} />
+          <Posts
+            key={index}
+            data={data}
+            read={read}
+            setRead={setRead}
+            setNumber={setNumber}
+            number={number}
+          />
         ))}
       </div>
     </div>
--- src/Posts.js
+++ src/Posts.js
@@ -1,18 +1,32 @@
 import "./post.css";
-export default function Posts({ data }) {
+export default function Posts(props) {
+  const { setRead, read, data, setNumber, number } = props;
+
   return (
-    <div>
-      <div className="post">
+    <div className={!data.read == false ? "postunread" : "postread"}>
+      <div
+        className="post"
+        onClick={() => {
+          setRead(!read);
+          data.read = true;
+          if (number == 0) {
+            setNumber((prevNum) => prevNum - 0);
+          } else {
+            setNumber((prevNum) => prevNum - 1);
+          }
+        }}
+      >
         <img src={data.author.img} alt="photo" />
-        <div className="text">
+        <p className="text">
           <span className="name">{data.author.name} </span>
           {data.text}
           <span className="message"> {data.msg}</span>
           {data.chess ? <span className="chess"> {data.chess}</span> : null}
-          {data.private ? <p className="private"> {data.private}</p> : null}
-          <br /> <span className="time">{data.time}</span>
-        </div>
+          {data.read == false ? <span className="dot"></span> : null}
+        </p>
       </div>
+      <p className="time">{data.time}</p>
+      {data.private ? <p className="private"> {data.private}</p> : null}
     </div>
   );
 }
